7ef743cd273cc07b4f9fac6383c53731b67c5a7f
Kaung Htet Naing <11kaungkaung99@gmail.com>
2020-04-13 23:08:15 +0630

    second

--- autoincrement0_9&A_Z/src/main.java
+++ autoincrement0_9&A_Z/src/main.java
@@ -1,9 +1,10 @@
+
 import java.util.Scanner;
 
 public class main {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter Any String ( eg: AA000000 ) : ");
+        System.out.print("Enter Any Code ( eg: AA000000 ) : ");
         String userInput=sc.next();
         char first= (char) userInput.charAt(0);
         char second= (char) userInput.charAt(1);
@@ -17,6 +18,8 @@ public class main {
 
             num=0;
         }
+
+        System.out.print("Next Code : ");
         System.out.print(first);
         System.out.print(second);
         System.out.print(String.format("%06d",num));
